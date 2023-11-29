6ecbbff5bf466cb799adea9a642bc2bccd4557be
dhwanimodi23 <dcmodi23052000@gmail.com>
2023-11-28 23:06:42 -0500

    added modified files

--- MoneyMakers/views.py
+++ MoneyMakers/views.py
@@ -1,13 +1,15 @@
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import UserLoginForm, AccountRegistrationForm,UserProfileForm,AssetSellForm, PasswordResetForm, AssetExchangeForm, FundsAdditionForm,UpdatePasswordForm
+from .forms import UserLoginForm, AccountRegistrationForm, UserProfileForm, AssetSellForm, PasswordResetForm, \
+    AssetExchangeForm, FundsAdditionForm, UpdatePasswordForm
 from django.urls import reverse
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
 from .models import AccountProfile, CryptoCurrency, UserAccount, CryptoTransaction, AccountActivity
 import matplotlib as mpl
 import random
+
 mpl.use('Agg')  # Use the 'Agg' backend, which is non-interactive and works well in various environments
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -24,6 +26,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from django.conf import settings
+
+
 def index(request):
     user_wishlist = []
     current_user = None
@@ -40,16 +44,15 @@ def index(request):
     top_cryptocurrencies = CryptoCurrency.objects.all().order_by('market_cap_rank')[:20]
 
     # print(coin_list)
-    
 
-    
     context = {
         'user': current_user,
         'coins': top_cryptocurrencies,
         'user_log': user_logged_in,
         'wish_list': user_wishlist
     }
-    return render(request, 'FrontEnd/index.html',context)
+    return render(request, 'FrontEnd/index.html', context)
+
 
 def login_user(request):
     if request.session.get('user_session_id'):
@@ -72,24 +75,25 @@ def login_user(request):
                     else:
                         messages.error(request, "The userid or password do not match.")
                         empty_form = UserLoginForm()
-                        context ={'form': empty_form}
+                        context = {'form': empty_form}
                         return render(request, 'FrontEnd/login.html', context)
                 else:
                     messages.error(request, "The userid or password do not match.")
                     empty_form = UserLoginForm()
-                    context ={'form': empty_form}
+                    context = {'form': empty_form}
                     return render(request, 'FrontEnd/login.html', context)
 
             except AccountProfile.DoesNotExist:
                 messages.error(request, "Please create an account.")
                 empty_form = UserLoginForm()
-                context ={'form': empty_form}
+                context = {'form': empty_form}
                 return render(request, 'FrontEnd/login.html', context)
 
     else:
         empty_form = UserLoginForm()
-        context ={'form': empty_form}
+        context = {'form': empty_form}
         return render(request, 'FrontEnd/login.html', context)
+
 
 def process_form(request):
     print("inside process form")
@@ -104,10 +108,11 @@ def process_form(request):
         # Handle GET requests or other HTTP methods if needed
         return render(request, 'FrontEnd/login.html')
 
+
 def handle_user_input(request):
     print("Entering form handling routine.")
     if request.method == 'POST':
-        
+
         user_name = request.POST.get('given_name', '')
         user_email = request.POST.get('email_address', '')
         print(f"Received data - Name: {user_name}, Email: {user_email}")
@@ -115,6 +120,7 @@ def handle_user_input(request):
     else:
 
         return render(request, 'FrontEnd/login.html')
+
 
 def user_signup(request):
     if request.session.get('user_session_id'):
@@ -133,15 +139,14 @@ def user_signup(request):
     return render(request, 'FrontEnd/signup.html', {'form': form})
 
 
-
 def send_forgotpassword_mail(request):
-
     # print("user: ",request.session.get('user_session_id'))
     if request.session.get('user_session_id'):
         return HttpResponseRedirect(reverse('MoneyMakers:index'))
     print("forgot pass clicked")
 
     return HttpResponseRedirect(reverse('MoneyMakers:forgotpassword'))
+
 
 def forgot_password(request):
     if request.session.get('user_session_id'):
@@ -150,7 +155,7 @@ def forgot_password(request):
         form = PasswordResetForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email_address']
-            print("email entered is: ",email)
+            print("email entered is: ", email)
             try:
 
                 email_user = AccountProfile.objects.get(email_address=email)
@@ -174,8 +179,6 @@ def forgot_password(request):
     else:
         form = PasswordResetForm()
     return render(request, 'FrontEnd/forgotpassword.html', {"form": form})
-
-
 
 
 def change_password(request):
@@ -215,81 +218,76 @@ def change_password(request):
 
     else:
         form = UpdatePasswordForm()
-    return render(request, 'FrontEnd/changepassword.html',{"form":form})
+    return render(request, 'FrontEnd/changepassword.html', {"form": form})
 
 
+def generate_crypto_chart(request, cryptocurrency_name):
+    # Retrieve cryptocurrency details
+    crypto = get_object_or_404(CryptoCurrency, name=cryptocurrency_name)
 
-
-
-
-def dynamic_Crypto(request, coin_name):
-
-    coin = get_object_or_404(CryptoCurrency, name=coin_name)
+    # Adjust the current price if it's less than $1
     plt.switch_backend('Agg')
-    if coin.current_price < 1:
-        coin.current_price = 10 + coin.current_price
-    base_value = int(coin.current_price)
+    if crypto.current_price < 1:
+        crypto.current_price += 10
+    initial_price = int(crypto.current_price)
 
-    # Generate 12 monthly values that depend on the base value, for example, fluctuate by up to 15%
-    monthly_changes = np.random.uniform(-0.15, 0.15, 12)
-    monthly_values = base_value * (1 + monthly_changes)
+    # Create fluctuating monthly prices based on the initial price
+    fluctuation_rates = np.random.uniform(-0.15, 0.15, 12)
+    monthly_prices = initial_price * (1 + fluctuation_rates)
 
-    # Generate a date range of the last 12 months
-    end_date = datetime.today()
-    start_date = end_date - timedelta(days=365)
-    dates = pd.date_range(start=start_date, periods=12, freq='MS')  # 'MS' stands for month start frequency
+    # Prepare date range for the past year
+    today = datetime.today()
+    year_ago = today - timedelta(days=365)
+    monthly_dates = pd.date_range(start=year_ago, periods=12, freq='MS')
 
-    # Create the plot
+    # Setup the plot
     fig, ax = plt.subplots(figsize=(10, 5))
-    ax.plot(dates, monthly_values, color='darkorange', marker='o', linewidth=2)
+    ax.plot(monthly_dates, monthly_prices, color='darkorange', marker='o', linewidth=2)
 
-    # Format the dates on the x-axis
+    # Customize the x-axis with date formatting
     ax.xaxis.set_major_locator(mdates.MonthLocator())
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
     plt.xticks(rotation=45)
 
-    # Set y-axis range to ±15% from the base value
-    y_min = base_value * (1 - 0.2)
-    y_max = base_value * (1 + 0.2)
-    ax.set_ylim([y_min, y_max])
+    # Define the y-axis limits
+    y_axis_min = initial_price * 0.8
+    y_axis_max = initial_price * 1.2
+    ax.set_ylim([y_axis_min, y_axis_max])
 
-   
-    ax.get_yaxis().set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    ax.spines['bottom'].set_visible(True)
-    
-    percentage_change = .15 * 100
-    # Use ax.text() to add annotations to the plot
-    ax.text(0.03, 0.97, f'USD${coin.current_price:,.2f}',
-            transform=ax.transAxes, fontsize=14,
-            verticalalignment='top', bbox=dict(boxstyle='round,pad=0.2', fc='white', alpha=0.3))
+    # Customize plot appearance
+    ax.yaxis.set_visible(False)
+    for spine in ['top', 'right', 'left']:
+        ax.spines[spine].set_visible(False)
 
-    ax.text(0.03, 0.92, f'↗USD${15:,.2f} ({percentage_change:.2f}%)',
-            transform=ax.transAxes, fontsize=12,
-            verticalalignment='top', bbox=dict(boxstyle='round,pad=0.2', fc='white', alpha=0.3))
+    # Add annotations for current price and changes
+    change_percentage = 15
+    ax.text(0.03, 0.97, f'USD${crypto.current_price:,.2f}', transform=ax.transAxes,
+            fontsize=14, verticalalignment='top',
+            bbox=dict(boxstyle='round,pad=0.2', fc='white', alpha=0.3))
 
-    # Remaining plot code...
+    ax.text(0.03, 0.92, f'↗USD${15:,.2f} ({change_percentage:.2f}%)',
+            transform=ax.transAxes, fontsize=12, verticalalignment='top',
+            bbox=dict(boxstyle='round,pad=0.2', fc='white', alpha=0.3))
 
-    # Convert plot to PNG image
-    buf = BytesIO()
-    plt.savefig(buf, format='png', bbox_inches='tight')
-    plt.close(fig)  # Close the figure to free memory
-    buf.seek(0)  # Rewind the buffer
+    # Convert the plot to a PNG image
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png', bbox_inches='tight')
+    plt.close(fig)
+    buffer.seek(0)
 
-    # Encode the image in base64 and close the buffer
-    image_base64 = base64.b64encode(buf.read()).decode('utf-8')
-    buf.close()
+    # Encode the image to base64
+    encoded_image = base64.b64encode(buffer.read()).decode('utf-8')
+    buffer.close()
 
-    # Construct the image src data URI
-    image_url = f"data:image/png;base64,{image_base64}"
+    # Prepare the image URL
+    image_src = f"data:image/png;base64,{encoded_image}"
     context = {
-        "coin": coin,
-        "data_uri": image_url
+        "crypto": crypto,
+        "chart_url": image_src
     }
 
     return render(request, 'FrontEnd/dynamicCrypto.html', context)
+
 
 def handle_user_profile(request):
     # Retrieve the user's session ID
@@ -299,10 +297,9 @@ def handle_user_profile(request):
         form = UserProfileForm(request.POST, request.FILES, instance=user)
         # print("---------------    --------------------------------------",form)
         if form.is_valid():
-        # Extracting user input from POST request
+            # Extracting user input from POST request
             current_user = AccountProfile.objects.get(id=session_id)
-           
-            
+
             current_user.email_address = form.cleaned_data['email_address']
             print(request.FILES['identification_image'])
             if 'identification_image' in request.FILES:
@@ -322,7 +319,8 @@ def handle_user_profile(request):
         user_wishlist = current_user.preferences
 
         # Render the profile template with user details and wishlist
-        return render(request, 'FrontEnd/profile.html', {'user': current_user, 'wish_list': user_wishlist, 'id': "profile-details"})
+        return render(request, 'FrontEnd/profile.html',
+                      {'user': current_user, 'wish_list': user_wishlist, 'id': "profile-details"})
 
 
 def update_user_password(request):
@@ -368,7 +366,6 @@ def remove_user_account(request):
     return redirect('/')
 
 
-
 def process_user_logout(request):
     request.session.flush()
     logout(request)
@@ -392,7 +389,6 @@ def display_user_wishlist(request):
 
 
 def include_in_wishlist(request, cryptocurrency_name):
-    
     session_user_id = request.session.get('user_session_id')
     if not session_user_id:
         messages.info(request, "Please log in to add items to your wishlist.")
@@ -409,7 +405,6 @@ def include_in_wishlist(request, cryptocurrency_name):
         messages.info(request, f"{cryptocurrency_name} is already in your wishlist.")
     print("Wishlist Updated:", current_user)
     return redirect('MoneyMakers:index')
-
 
 
 def process_add_money(request):
@@ -439,7 +434,7 @@ def process_add_money(request):
                 transaction_amount=deposit_amount,
                 activity_type='deposit',
             )
-# Inform user of successful addition
+            # Inform user of successful addition
             messages.success(request, "Funds added successfully.")
         else:
             # Handle form errors
@@ -454,10 +449,11 @@ def process_add_money(request):
     # Get the current balance to display
     current_balance = user_wallet.account_balance
     return render(request, 'FrontEnd/profile.html', {
-        'form': money_addition_form, 
-        'balance': current_balance, 
+        'form': money_addition_form,
+        'balance': current_balance,
         'id': "process_add_money"
     })
+
 
 def execute_currency_purchase(request):
     # Authenticate user
@@ -480,7 +476,7 @@ def execute_currency_purchase(request):
             transaction_type = purchase_form.cleaned_data['action_type']
             total_cost = chosen_crypto.current_price_cad * purchase_quantity
             print(transaction_type)
-            
+
             # Check if the user has enough balance
             if user_wallet.account_balance >= total_cost:
                 # Deduct the amount from the wallet
@@ -500,17 +496,17 @@ def execute_currency_purchase(request):
                 user_crypto_holdings = user_profile.digital_assets
 
                 # Add or update the quantity of purchased cryptocurrency
-                user_crypto_holdings[chosen_crypto.name] = user_crypto_holdings.get(chosen_crypto.name, 0) + int(purchase_quantity)
+                user_crypto_holdings[chosen_crypto.name] = user_crypto_holdings.get(chosen_crypto.name, 0) + int(
+                    purchase_quantity)
 
                 # Save updates to user profile and wallet
                 user_profile.digital_assets = user_crypto_holdings
                 user_wallet.save()
                 user_profile.save()
 
-    
                 messages.success(request, "Funds added successfully.")
             else:
-            # Handle form errors
+                # Handle form errors
                 messages.error(request, "Error in form submission.")
         else:
             # Handle form errors
@@ -524,16 +520,16 @@ def execute_currency_purchase(request):
         purchase_form = AssetExchangeForm(current_user_id)
 
         # Prepare cryptocurrency choices for the template
-        crypto_options = [{'id': crypto.id, 'name': crypto.name, 'price': str(crypto.current_price_cad)} 
+        crypto_options = [{'id': crypto.id, 'name': crypto.name, 'price': str(crypto.current_price_cad)}
                           for crypto in CryptoCurrency.objects.all()]
         crypto_options_json = json.dumps(crypto_options, cls=DjangoJSONEncoder)
 
         # Render the purchase page
         return render(request, 'FrontEnd/profile.html', {
-            'form': purchase_form, 
-            'balance': user_wallet.account_balance, 
+            'form': purchase_form,
+            'balance': user_wallet.account_balance,
             'crypto_choices_json': crypto_options_json,
-            'type':'buy',
+            'type': 'buy',
             'id': "purchase-currency"
         })
 
@@ -566,7 +562,8 @@ def execute_currency_sell(request):
             if int(user_crypto_holdings[chosen_crypto.name]) >= int(purchase_quantity):
                 # Deduct the amount from the wallet
                 user_wallet.account_balance += total_cost
-                user_crypto_holdings[chosen_crypto.name] = user_crypto_holdings.get(chosen_crypto.name, 0) - int(purchase_quantity)
+                user_crypto_holdings[chosen_crypto.name] = user_crypto_holdings.get(chosen_crypto.name, 0) - int(
+                    purchase_quantity)
 
                 # Record the purchase
                 CryptoTransaction.objects.create(
@@ -576,19 +573,18 @@ def execute_currency_sell(request):
                     volume=purchase_quantity,
                     total_value=total_cost,
                 )
-                if int(user_crypto_holdings[chosen_crypto.name]) <=0:
+                if int(user_crypto_holdings[chosen_crypto.name]) <= 0:
                     del user_crypto_holdings[chosen_crypto.name]
                 # Update user's cryptocurrency holdings
-               
+
                 # Save updates to user profile and wallet
                 user_profile.digital_assets = user_crypto_holdings
                 user_wallet.save()
                 user_profile.save()
 
-    
                 messages.success(request, "Funds added successfully.")
             else:
-            # Handle form errors
+                # Handle form errors
                 messages.error(request, "Error in form submission.")
         else:
             # Handle form errors
@@ -603,18 +599,19 @@ def execute_currency_sell(request):
         user_profile = AccountProfile.objects.get(id=current_user_id)
         user_crypto_holdings = user_profile.digital_assets
         crypto_ids = list(user_crypto_holdings.keys())
-        crypto_options = [{'id': crypto.id, 'name': crypto.name, 'price': str(crypto.current_price_cad)} 
+        crypto_options = [{'id': crypto.id, 'name': crypto.name, 'price': str(crypto.current_price_cad)}
                           for crypto in CryptoCurrency.objects.filter(name__in=crypto_ids)]
         crypto_options_json = json.dumps(crypto_options, cls=DjangoJSONEncoder)
         crypto_holdings_json = json.dumps(user_crypto_holdings, cls=DjangoJSONEncoder)
         return render(request, 'FrontEnd/profile.html', {
-            'form': purchase_form, 
-            'balance': user_wallet.account_balance, 
+            'form': purchase_form,
+            'balance': user_wallet.account_balance,
             'crypto_chs_json': crypto_options_json,
             'crypto_holdings': crypto_holdings_json,
             'id': "sell-currency"
         })
-   
+
+
 def user_purchaseses(request):
     user_id = request.session.get('user_session_id')
     if not user_id:
@@ -623,7 +620,7 @@ def user_purchaseses(request):
     user_purchases = CryptoTransaction.objects.filter(user=user_id)
     print("user purchases", user_purchases)
     return render(request, 'FrontEnd/profile.html', {
-            'user_purchases': user_purchases, 
-            
-            'id': "user_purchases"
-        })
+        'user_purchases': user_purchases,
+
+        'id': "user_purchases"
+    })
